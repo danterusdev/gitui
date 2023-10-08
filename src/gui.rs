67774037636159e7a22a49d4541c495421234e89
danterusdev <danterusdev@gmail.com>
2023-10-07 21:41:43 -0600

    Add zooming

--- src/gui.rs
+++ src/gui.rs
@@ -5,7 +5,7 @@ use git2::Repository;
 use iced::advanced::mouse::Cursor;
 use iced::alignment::{Horizontal, Vertical};
 use iced::event::Status;
-use iced::mouse::{Button, Interaction};
+use iced::mouse::{Button, Interaction, ScrollDelta};
 use iced::widget::canvas::{Program, Geometry, Frame, Path, Style, Text, Stroke, Event};
 use iced::widget::{text, Column, Row, Canvas};
 use iced::{Alignment, Element, Sandbox, Settings, Length, Rectangle, Theme, Color, mouse, Renderer, Point, Vector};
@@ -145,39 +145,48 @@ fn get_commit_node_location(commit: &CommitNode, commits: &HashMap<String, Commi
 }
 
 fn adjust_position_for_view(position: Point, bounds: &Rectangle, state: &TreeState) -> Point {
-    Point::new(position.x + bounds.width / 2.0 + state.offset.x, position.y + bounds.height / 2.0 + state.offset.y)
+    let x = state.zoom * (position.x + state.offset.x) + bounds.width / 2.0;
+    let y = state.zoom * (position.y + state.offset.y) + bounds.height / 2.0;
+    Point::new(x, y)
 }
 
-#[derive(Default)]
 struct TreeState {
     mouse_location: Point,
     dragging: bool,
     offset_start: Vector,
     dragging_start: Point,
     offset: Vector,
+    zoom: f32,
+}
+
+impl Default for TreeState {
+    fn default() -> Self {
+        Self {
+            mouse_location: Default::default(),
+            dragging: Default::default(),
+            offset_start: Default::default(),
+            dragging_start: Default::default(),
+            offset: Default::default(),
+            zoom: 1.0,
+        }
+    }
 }
 
 impl Program<Message> for TreeRenderer {
     type State = TreeState;
 
-    fn update(
-            &self,
-            state: &mut Self::State,
-            event: Event,
-            bounds: Rectangle,
-            _cursor: Cursor,
-        ) -> (Status, Option<Message>) {
+    fn update(&self, state: &mut Self::State, event: Event, bounds: Rectangle, _cursor: Cursor) -> (Status, Option<Message>) {
         let commits = &self.state.borrow().commits;
 
         match event {
             Event::Mouse(mouse::Event::ButtonPressed(button)) => {
                 if button == Button::Left {
                     if state.mouse_location.y > 0.0 {
-                        for commit in self.state.borrow().commits.values() {
+                        for commit in commits.values() {
                             let location = get_commit_node_location(commit, commits);
                             let location = adjust_position_for_view(location, &bounds, state);
 
-                            if state.mouse_location.distance(location) < NODE_RADIUS {
+                            if state.mouse_location.distance(location) < NODE_RADIUS * state.zoom {
                                 return (Status::Captured, Some(Message::SelectCommit(commit.id.clone())))
                             }
                         }
@@ -209,25 +218,76 @@ impl Program<Message> for TreeRenderer {
                 state.mouse_location = Point::new(location.x - bounds.x, location.y - bounds.y);
 
                 if state.dragging {
-                    state.offset = state.offset_start + (state.mouse_location - state.dragging_start);
+                    state.offset = state.offset_start + (state.mouse_location - state.dragging_start) * (1.0 / state.zoom);
                 }
 
                 (Status::Captured, None)
-            }
+            },
+            Event::Mouse(mouse::Event::WheelScrolled { delta }) => {
+                if !state.dragging {
+                    if let ScrollDelta::Lines { x: _, y } = delta {
+                        // want to get the distance the origin changes from the scroll, then
+                        // subtract it
+                        //
+                        // get mouse location and subtract center to get distance between them
+                        // multiply that by zoom
+
+                        //let x = state.mouse_location.x - bounds.width / 2.0;
+
+                        state.zoom += y * 0.15 * state.zoom;
+                        state.zoom = state.zoom.clamp(0.25, 4.0);
+
+                        //let xReal = state.offset.x;//(state.mouse_location.x - bounds.width / 2.0) * previous;
+
+                        // Calculate the XY as if the element is in its original, non-scaled size:
+                        //let xOrg = xReal / previous;
+
+                        // Calculate the scaled XY
+                        //let xNew = xOrg * state.zoom;  // PS: scale here is the new scale.
+
+                        // Retrieve the XY difference to be used as the change in offset.
+                        //let xDiff = xReal - xNew;
+
+                        //state.offset.x += xDiff;
+                        //let target = (state.mouse_location.x - (bounds.width / 2.0 + state.offset.x)) / previous;
+
+                        //state.offset.x = -target * state.zoom;
+
+
+                        //println!("{}", ((state.offset.x * previous + state.mouse_location.x) - (state.offset.x * state.zoom + bounds.width / 2.0)) * (state.zoom - previous) * state.zoom);
+                        //state.offset.x -= (state.mouse_location.x - bounds.width / 2.0) / previous / 3.0 * (state.zoom - previous);
+                        //state.offset.x -= ((state.offset.x * previous + state.mouse_location.x) - (state.offset.x * state.zoom + bounds.width / 2.0)) * (state.zoom - previous) * state.zoom;
+
+                        //state.offset.x -= ((state.offset.x + state.mouse_location.x) - (state.offset.x + bounds.width / 2.0)) * (state.zoom - previous) * state.zoom;
+                        //state.offset.y -= ((state.offset.y + state.mouse_location.y) - (state.offset.y + bounds.height / 2.0)) * (state.zoom - previous) * state.zoom;
+                    }
+                }
+                (Status::Captured, None)
+            },
             _ => (Status::Ignored, None),
         }
     }
 
-    fn mouse_interaction(
-            &self,
-            state: &Self::State,
-            _bounds: Rectangle,
-            _cursor: Cursor,
-        ) -> Interaction {
+    fn mouse_interaction(&self, state: &Self::State, bounds: Rectangle, _cursor: Cursor) -> Interaction {
+        let commits = &self.state.borrow().commits;
+
         if state.dragging {
             Interaction::Grabbing
         } else {
-            Interaction::Grab
+            for commit in commits.values() {
+                let location = get_commit_node_location(commit, commits);
+                let location = adjust_position_for_view(location, &bounds, state);
+
+                if state.mouse_location.distance(location) < NODE_RADIUS * state.zoom {
+                    return Interaction::Pointer
+                }
+            }
+
+            if state.mouse_location.y > 0.0 {
+                Interaction::Pointer
+            } else {
+                Interaction::Idle
+            }
         }
     }
 
@@ -240,13 +300,13 @@ impl Program<Message> for TreeRenderer {
             let location = get_commit_node_location(commit, commits);
             let location = adjust_position_for_view(location, &bounds, state);
 
-            let node = Path::circle(location, NODE_RADIUS);
+            let node = Path::circle(location, NODE_RADIUS * state.zoom);
             frame.fill(&node, Color::from_rgb(0.35, 0.35, 0.35));
 
             let text = Text {
                 content: commit.id[..6].to_string(),
                 position: location,
-                size: 15.0,
+                size: 15.0 * state.zoom,
                 color: Color::from_rgb(0.8, 0.8, 0.8),
                 horizontal_alignment: Horizontal::Center,
                 vertical_alignment: Vertical::Center,
@@ -258,7 +318,7 @@ impl Program<Message> for TreeRenderer {
             if commit.parents.len() > 0 {
                 let parent_location = get_commit_node_location(commits.get(commit.parents.get(0).unwrap()).unwrap(), commits);
                 let parent_location = adjust_position_for_view(parent_location, &bounds, state);
-                let path = Path::line(Point::new(location.x - NODE_RADIUS, location.y), Point::new(parent_location.x + NODE_RADIUS, parent_location.y));
+                let path = Path::line(Point::new(location.x - NODE_RADIUS * state.zoom, location.y), Point::new(parent_location.x + NODE_RADIUS * state.zoom, parent_location.y));
                 frame.stroke(&path, Stroke {
                     width: 2.0,
                     style: Style::Solid(Color::BLACK),
